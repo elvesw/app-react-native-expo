fb6c8de05133c9f0f3948fc1b373e5d3813404bc
elves <elvesw@gmail.com>
2021-07-11 21:27:29 -0300

    add husky and config

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Routes: React.FC = () => {
+	return (
+		<View>
+			<Text style={{ color: '#BDC3C7', fontSize: 18 }}>
+				Primeiro screen teste
+			</Text>
+		</View>
+	);
+};
+
+export default Routes;
